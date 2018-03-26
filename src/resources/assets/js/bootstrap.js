05df3997c400efd0da724391e932937afddb00d9
wilyxjam <wilyxjam@gmail.com>
2018-03-26 17:00:36 +0200

    add js/css libs imports

--- src/resources/assets/js/bootstrap.js
+++ src/resources/assets/js/bootstrap.js
@@ -7,11 +7,11 @@ window._ = require('lodash');
  * code may be modified to fit the specific needs of your application.
  */
 
-try {
-    window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
-} catch (e) {}
+// try {
+//     window.$ = window.jQuery = require('jquery');
+//
+//     require('bootstrap-sass');
+// } catch (e) {}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -53,3 +53,8 @@ if (token) {
 //     cluster: 'mt1',
 //     encrypted: true
 // });
+
+require('jquery');
+require('popper.js');
+require('bootstrap');
+require('leaflet');
