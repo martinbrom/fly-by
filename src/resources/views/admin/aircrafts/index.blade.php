19a35485a19c9b06fe4e3a715c6cd56ca6b02109
Martin Brom <martinbrom97@gmail.com>
2018-04-08 17:53:52 +0200

    aircraft&airport admin index layout

--- src/resources/views/admin/aircrafts/index.blade.php
+++ src/resources/views/admin/aircrafts/index.blade.php
@@ -6,20 +6,45 @@
 
     <a href="{{ route('admin.aircrafts.create') }}">Add a new aircraft</a>
 
-    @foreach($aircrafts as $aircraft)
-        <div class="aircraft">
-            <h3 class="aircraft-name">{{ $aircraft->name }}</h3>
-            <a href="{{ route('admin.aircrafts.show', $aircraft->id) }}">Display aircraft</a>
-            <a href="{{ route('admin.aircrafts.edit', $aircraft->id) }}">Edit aircraft</a><br>
-            <p class="aircraft-range">Range: {{ $aircraft->range }} km</p>
-            <p class="aircraft-speed">Speed: {{ $aircraft->speed }} km/h</p>
-            <p class="aircraft-cost">Cost: {{ $aircraft->cost }} CZK/h</p>
+    @if(count($aircrafts) > 0)
+        <table class="table table-striped">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Range (km)</th>
+                    <th scope="col">Speed (km/h)</th>
+                    <th scope="col">Cost (CZK/h)</th>
+                    <th scope="col">Display</th>
+                    <th scope="col">Edit</th>
+                    <th scope="col">Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($aircrafts as $aircraft)
+                    <tr>
+                        <td class="aircraft-id">{{ $aircraft->id }}</td>
+                        <td class="aircraft-name">{{ $aircraft->name }}</td>
+                        <td class="aircraft-range">{{ $aircraft->range }}</td>
+                        <td class="aircraft-speed">{{ $aircraft->speed }}</td>
+                        <td class="aircraft-cost">{{ $aircraft->cost }}</td>
+                        <td class="aircraft-show"><a href="{{ route('admin.aircrafts.show', $aircraft->id) }}">Display</a></td>
+                        <td class="aircraft-edit"><a href="{{ route('admin.aircrafts.edit', $aircraft->id) }}">Edit</a><br></td>
 
-            <!-- TODO: Confirm dialog -->
-            {{ Form::open(['route' => ['admin.aircrafts.destroy', $aircraft->id], 'method' => 'delete']) }}
-                <button type="submit" >Delete aircraft</button>
-            {{ Form::close() }}
-        </div>
-    @endforeach
+                        <td class="aircraft-destroy">
+                            <!-- TODO: Confirm dialog -->
+                            <a href="#aircraft-delete-form-{{ $aircraft->id }}" onclick="event.preventDefault(); document.getElementById('aircraft-delete-form-{{ $aircraft->id }}').submit();">
+                                Delete
+                            </a>
+                            <form id="aircraft-delete-form-{{ $aircraft->id }}" action="{{ route('admin.aircrafts.destroy', $aircraft->id) }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                                {{ method_field('DELETE') }}
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
 
 @endsection
--- src/resources/views/admin/airports/index.blade.php
+++ src/resources/views/admin/airports/index.blade.php
@@ -6,8 +6,45 @@
 
     <a href="{{ route('admin.airports.create') }}">Add a new airport</a>
 
-    @foreach($airports as $airport)
-        <a href="{{ route('admin.airports.show', $airport->id) }}">{{ $airport->name }}</a>
-    @endforeach
+    @if(count($airports) > 0)
+        <table class="table table-striped">
+            <thead class="thead-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Latitude</th>
+                <th scope="col">Longitude</th>
+                <th scope="col">Code</th>
+                <th scope="col">Display</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($airports as $airport)
+                <tr>
+                    <td class="airport-id">{{ $airport->id }}</td>
+                    <td class="airport-name">{{ $airport->name }}</td>
+                    <td class="airport-latitude">{{ $airport->lat }}</td>
+                    <td class="airport-longitude">{{ $airport->lon }}</td>
+                    <td class="airport-code">{{ $airport->code }}</td>
+                    <td class="airport-show"><a href="{{ route('admin.airports.show', $airport->id) }}">Display</a></td>
+                    <td class="airport-edit"><a href="{{ route('admin.airports.edit', $airport->id) }}">Edit</a><br></td>
+
+                    <td class="airport-destroy">
+                        <!-- TODO: Confirm dialog -->
+                        <a href="#airport-delete-form-{{ $airport->id }}" onclick="event.preventDefault(); document.getElementById('airport-delete-form-{{ $airport->id }}').submit();">
+                            Delete
+                        </a>
+                        <form id="airport-delete-form-{{ $airport->id }}" action="{{ route('admin.airports.destroy', $airport->id) }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                            {{ method_field('DELETE') }}
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    @endif
 
 @endsection
